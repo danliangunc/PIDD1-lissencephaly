001367ff19a9afe4e7fb6d07f6d4ec7b21957bf1
danliangunc <liangdan312@hotmail.com>
2024-09-29 17:27:19 -0400

    Add files via upload

---
+++ Scripts/SupData/Run_topGO_UpperLayerNeuron.R
@@ -0,0 +1,155 @@
+##Set up the input files for topGO for Hydrocephalus Risk gene and Microcephaly Risk gene
+library(GenomicRanges);
+library(GenomicFeatures);
+library(GenomicAlignments);
+library(biomaRt);
+library(AnnotationDbi);
+library(org.Hs.eg.db);
+library(topGO);
+options(stringsAsFactors=FALSE);
+## DEG genes
+AstrDEG = read.csv("../Merged_SeuratAnalysis/Astrocyte_DEG_C_vs_CT.csv",row.names=1);
+## C gene list
+AstrDEG.C = AstrDEG[which(AstrDEG$cluster=="C"),];
+AstrDEG.C = AstrDEG.C[which(AstrDEG.C$p_val_adj < 0.05 & AstrDEG.C$avg_log2FC > 0.3),];
+## P gene list
+AstrDEG.CT = AstrDEG[which(AstrDEG$cluster=="CT"),];
+AstrDEG.CT = AstrDEG.CT[which(AstrDEG.CT$p_val_adj < 0.05 & AstrDEG.CT$avg_log2FC > 0.3),];
+## find protein coding promoters and non-protein coding promoters
+load("/home/dl2248/project_old/CoWithDuyPhan/GeneOntology/hgncdata+hg38.Rdata");
+codgene=hgncdata[which(hgncdata$gene_biotype=="protein_coding"),];
+##All genes
+geneNames= unique(codgene$entrezgene_id);
+##C gene gene
+output=unique(codgene$entrezgene_id[which(codgene$hgnc_symbol %in% AstrDEG.C$gene)]);
+myInterestingGenes = output;
+geneList <- factor(as.integer(geneNames %in% myInterestingGenes));
+names(geneList) <- geneNames;
+C_GOdata <- new("topGOdata",ontology = "BP",description = "UpperLayerNeuron Control Up Regulated Genes",allGenes = geneList,nodeSize = 10,annotationFun = annFUN.org,mapping = "org.Hs.eg.db");
+resultFisher <- runTest(C_GOdata, algorithm = "classic", statistic = "fisher");
+CallRes <- GenTable(C_GOdata, classicFisher = resultFisher,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = 25);
+## P genes
+output=unique(codgene$entrezgene_id[which(codgene$hgnc_symbol %in% AstrDEG.CT$gene)]);
+myInterestingGenes = output;
+geneList <- factor(as.integer(geneNames %in% myInterestingGenes));
+names(geneList) <- geneNames;
+P_GOdata <- new("topGOdata",ontology = "BP",description = "UpperLayerNeuron Control-Treatment Up Regulated Genes",allGenes = geneList,nodeSize = 10,annotationFun = annFUN.org,mapping = "org.Hs.eg.db");
+resultFisher <- runTest(P_GOdata, algorithm = "classic", statistic = "fisher");
+PallRes <- GenTable(P_GOdata, classicFisher = resultFisher,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = 25);
+## save resutls
+CallRes$C_vs_CT = "C";
+PallRes$C_vs_CT = "CT";
+output = rbind(CallRes,PallRes);
+write.csv(output, file = "Top_GO_UpperLayerNeuron_results_C_vs_CT.csv",quote=F,row.names=F);
+## plot
+pdf(file="TopGO_UpperLayerNeuron_DEGene_C_vs_CT.pdf",height=8,width=6);
+par(oma=c(2,10,1,1));
+    bp = barplot(-log10(as.numeric(CallRes$classicFisher)),main="UpperLayerNeuron Control Up Regulated Genes",horiz=TRUE,yaxt='n',yaxt='n',col="blue",xlab="-log10(P-value)",cex.main=0.5);
+    axis(2,at=bp,labels=CallRes$Term,tick=FALSE,las=2,cex.axis=0.5);
+    abline(v=-log(0.01),col="red",lwd=2,lty=1);
+
+    bp = barplot(-log10(as.numeric(PallRes$classicFisher)),main="UpperLayerNeuron Control-Treatment Up Regulated Genes",horiz=TRUE,yaxt='n',yaxt='n',col="blue",xlab="-log10(P-value)",cex.main=0.5);
+    axis(2,at=bp,labels=PallRes$Term,tick=FALSE,las=2,cex.axis=0.5);
+    abline(v=-log(0.01),col="red",lwd=2,lty=1);
+
+dev.off();
+
+
+
+
+
+AstrDEG = read.csv("../Merged_SeuratAnalysis/Astrocyte_DEG_P_vs_PT.csv",row.names=1);
+## C gene list
+AstrDEG.C = AstrDEG[which(AstrDEG$cluster=="P"),];
+AstrDEG.C = AstrDEG.C[which(AstrDEG.C$p_val_adj < 0.05 & AstrDEG.C$avg_log2FC > 0.3),];
+## P gene list
+AstrDEG.CT = AstrDEG[which(AstrDEG$cluster=="PT"),];
+AstrDEG.CT = AstrDEG.CT[which(AstrDEG.CT$p_val_adj < 0.05 & AstrDEG.CT$avg_log2FC > 0.3),];
+## find protein coding promoters and non-protein coding promoters
+load("/home/dl2248/project_old/CoWithDuyPhan/GeneOntology/hgncdata+hg38.Rdata");
+codgene=hgncdata[which(hgncdata$gene_biotype=="protein_coding"),];
+##All genes
+geneNames= unique(codgene$entrezgene_id);
+##C gene gene
+output=unique(codgene$entrezgene_id[which(codgene$hgnc_symbol %in% AstrDEG.C$gene)]);
+myInterestingGenes = output;
+geneList <- factor(as.integer(geneNames %in% myInterestingGenes));
+names(geneList) <- geneNames;
+C_GOdata <- new("topGOdata",ontology = "BP",description = "UpperLayerNeuron Patient Up Regulated Genes",allGenes = geneList,nodeSize = 10,annotationFun = annFUN.org,mapping = "org.Hs.eg.db");
+resultFisher <- runTest(C_GOdata, algorithm = "classic", statistic = "fisher");
+CallRes <- GenTable(C_GOdata, classicFisher = resultFisher,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = 25);
+## P genes
+output=unique(codgene$entrezgene_id[which(codgene$hgnc_symbol %in% AstrDEG.CT$gene)]);
+myInterestingGenes = output;
+geneList <- factor(as.integer(geneNames %in% myInterestingGenes));
+names(geneList) <- geneNames;
+P_GOdata <- new("topGOdata",ontology = "BP",description = "UpperLayerNeuron Patient-Treatment Up Regulated Genes",allGenes = geneList,nodeSize = 10,annotationFun = annFUN.org,mapping = "org.Hs.eg.db");
+resultFisher <- runTest(P_GOdata, algorithm = "classic", statistic = "fisher");
+PallRes <- GenTable(P_GOdata, classicFisher = resultFisher,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = 25);
+## save resutls
+CallRes$P_vs_PT = "P";
+PallRes$P_vs_PT = "PT";
+output = rbind(CallRes,PallRes);
+write.csv(output, file = "Top_GO_UpperLayerNeuron_results_P_vs_PT.csv",quote=F,row.names=F);
+## plot
+pdf(file="TopGO_UpperLayerNeuron_DEGene_P_vs_PT.pdf",height=8,width=6);
+par(oma=c(2,10,1,1));
+    bp = barplot(-log10(as.numeric(CallRes$classicFisher)),main="UpperLayerNeuron Patient Up Regulated Genes",horiz=TRUE,yaxt='n',yaxt='n',col="blue",xlab="-log10(P-value)",cex.main=0.5);
+    axis(2,at=bp,labels=CallRes$Term,tick=FALSE,las=2,cex.axis=0.5);
+    abline(v=-log(0.01),col="red",lwd=2,lty=1);
+
+    bp = barplot(-log10(as.numeric(PallRes$classicFisher)),main="UpperLayerNeuron Patient-Treatment Up Regulated Genes",horiz=TRUE,yaxt='n',yaxt='n',col="blue",xlab="-log10(P-value)",cex.main=0.5);
+    axis(2,at=bp,labels=PallRes$Term,tick=FALSE,las=2,cex.axis=0.5);
+    abline(v=-log(0.01),col="red",lwd=2,lty=1);
+
+dev.off();
+
+
+AstrDEG = read.csv("../Merged_SeuratAnalysis/Astrocyte_DEG_MDS_vs_MDSD.csv",row.names=1);
+## C gene list
+AstrDEG.C = AstrDEG[which(AstrDEG$cluster=="MDS"),];
+AstrDEG.C = AstrDEG.C[which(AstrDEG.C$p_val_adj < 0.05 & AstrDEG.C$avg_log2FC > 0.3),];
+## P gene list
+AstrDEG.CT = AstrDEG[which(AstrDEG$cluster=="MDSD"),];
+AstrDEG.CT = AstrDEG.CT[which(AstrDEG.CT$p_val_adj < 0.05 & AstrDEG.CT$avg_log2FC > 0.3),];
+## find protein coding promoters and non-protein coding promoters
+load("/home/dl2248/project_old/CoWithDuyPhan/GeneOntology/hgncdata+hg38.Rdata");
+codgene=hgncdata[which(hgncdata$gene_biotype=="protein_coding"),];
+##All genes
+geneNames= unique(codgene$entrezgene_id);
+##C gene gene
+output=unique(codgene$entrezgene_id[which(codgene$hgnc_symbol %in% AstrDEG.C$gene)]);
+myInterestingGenes = output;
+geneList <- factor(as.integer(geneNames %in% myInterestingGenes));
+names(geneList) <- geneNames;
+C_GOdata <- new("topGOdata",ontology = "BP",description = "UpperLayerNeuron MDS Up Regulated Genes",allGenes = geneList,nodeSize = 10,annotationFun = annFUN.org,mapping = "org.Hs.eg.db");
+resultFisher <- runTest(C_GOdata, algorithm = "classic", statistic = "fisher");
+CallRes <- GenTable(C_GOdata, classicFisher = resultFisher,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = 25);
+## P genes
+output=unique(codgene$entrezgene_id[which(codgene$hgnc_symbol %in% AstrDEG.CT$gene)]);
+myInterestingGenes = output;
+geneList <- factor(as.integer(geneNames %in% myInterestingGenes));
+names(geneList) <- geneNames;
+P_GOdata <- new("topGOdata",ontology = "BP",description = "UpperLayerNeuron MDS-Treatment Up Regulated Genes",allGenes = geneList,nodeSize = 10,annotationFun = annFUN.org,mapping = "org.Hs.eg.db");
+resultFisher <- runTest(P_GOdata, algorithm = "classic", statistic = "fisher");
+PallRes <- GenTable(P_GOdata, classicFisher = resultFisher,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = 25);
+## save resutls
+CallRes$MDS_vs_MDSD = "MDS";
+PallRes$MDS_vs_MDSD = "MDSD";
+output = rbind(CallRes,PallRes);
+write.csv(output, file = "Top_GO_UpperLayerNeuron_results_MDS_vs_MDSD.csv",quote=F,row.names=F);
+## plot
+pdf(file="TopGO_UpperLayerNeuron_DEGene_MDS_vs_MDSD.pdf",height=8,width=6);
+par(oma=c(2,10,1,1));
+    bp = barplot(-log10(as.numeric(CallRes$classicFisher)),main="UpperLayerNeuron MDS Up Regulated Genes",horiz=TRUE,yaxt='n',yaxt='n',col="blue",xlab="-log10(P-value)",cex.main=0.5);
+    axis(2,at=bp,labels=CallRes$Term,tick=FALSE,las=2,cex.axis=0.5);
+    abline(v=-log(0.01),col="red",lwd=2,lty=1);
+
+    bp = barplot(-log10(as.numeric(PallRes$classicFisher)),main="UpperLayerNeuron MDS-Treatment Up Regulated Genes",horiz=TRUE,yaxt='n',yaxt='n',col="blue",xlab="-log10(P-value)",cex.main=0.5);
+    axis(2,at=bp,labels=PallRes$Term,tick=FALSE,las=2,cex.axis=0.5);
+    abline(v=-log(0.01),col="red",lwd=2,lty=1);
+
+dev.off();
+
+
+
